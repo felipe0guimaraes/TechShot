70d13a977896cd691a50a950dcccabe9268c86f7
felipe0guimaraes <felipeoguimaraes@hotmail.com>
2016-08-23 00:54:55 -0300

    commit 2

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello!");
+            Console.Write("Hello World!");
         }
     }
 }
